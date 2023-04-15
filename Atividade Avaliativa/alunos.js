38c7015660710194c8b5572eacc5b672a8f4562a
luucdamaso <lucas.s.damaso@hotmail.com>
2023-04-15 20:56:19 -0300

    Atividade avaliativa

--- Atividade Avaliativa/alunos.js
+++ Atividade Avaliativa/alunos.js
@@ -1,9 +1,21 @@
-const alunos = [
-  { nome: 'João', media: 7.5 },
-  { nome: 'Maria', media: 8.2 },
-  { nome: 'Pedro', media: 6.9 },
-  { nome: 'Ana', media: 9.1 },
-  { nome: 'Thiago', media: 5.4 }
-];
-
-module.exports= alunos;
+const alunosChamada = [
+  { nome: 'João', matricula: 582, media: 7.5 },
+  { nome: 'Maria', matricula: 362, media: 8.2 },
+  { nome: 'Pedro', matricula: 741, media: 6.9 },
+  { nome: 'Ana', matricula: 957, media: 9.1 },
+  { nome: 'Thiago', matricula: 203, media: 5.4 },
+  ];
+  
+  function adicionarAluno(aluno) {
+    alunosChamada.push(aluno);
+  }
+  
+  function deletarAluno(index) {
+    alunosChamada.splice(index, 1);
+  }
+  
+  function atualizarAluno(index, alunoAtualizado) {
+    alunosChamada[index] = alunoAtualizado;
+  }
+  
+  module.exports = { alunosChamada, adicionarAluno, deletarAluno, atualizarAluno };
--- Atividade Avaliativa/index.js
+++ Atividade Avaliativa/index.js
@@ -1,51 +1,96 @@
 const express = require("express");
-const alunos = require("./alunos.js")
-
+const { alunosChamada, adicionarAluno, deletarAluno, atualizarAluno } = require("./alunos");
+const fs = require("fs");
 const app = express();
 app.use(express.json());
 
+const dbFilePath = "./db.json";
+let alunos = [];
+
+
+fs.readFile(dbFilePath, (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+        alunos = JSON.parse(data);
+    }
+});
+
 app.get("/alunos", (req, res) => {
-    const { aluno, media } = req.query;
+    const { aluno, matricula, media } = req.query;
 
     if (aluno) {
-        let nomeAluno = alunos.filter(a => a.nome.toLowerCase().includes(aluno.toLowerCase()));
+        let nomeAluno = alunosChamada.filter(a => a.nome.toLowerCase().includes(aluno.toLowerCase()));
         res.json(nomeAluno)
     }
+    if (matricula) {
+      let matriculaAluno = alunosChamada.filter(a => a.matricula === Number(matricula));
+      res.json(matriculaAluno)
+  }
     if (media) {
-        let mediaAluno = alunos.filter(a => a.media === Number(media));
+        let mediaAluno = alunosChamada.filter(a => a.media === Number(media));
         res.json(mediaAluno)
     }
+
     res.json(alunos);
 });
 
-app.post("/alunos/novo", (req, res) =>{
+app.put("/alunos/novo", (req, res) =>{
     const {nome, matricula, media} = req.body;
     if((nome !== undefined )||(matricula !== undefined)||(media !== undefined)){
-        res.json(`${nome} você foi adicionado`)
+      adicionarAluno({ nome, matricula, media });
+        fs.writeFile(dbFilePath, JSON.stringify(alunos), err => {
+            if (err) {
+                res.status(500).json({message: "Erro ao escrever no arquivo"});
+            } else {
+                res.json(`${nome} você foi adicionado`);
+            }
+        });
     }else {
         res.status(400).json({message: "Não foi possivel adicionar"})
     }
 });
 
-app.post("/alunos/deletar/:index", (req, res) =>{
-    const {aluno} = req.body;
-    if(nome !== undefined ){
-        res.json(`${aluno} você foi excluido`)
+app.delete("/alunos/deletar/:index", (req, res) =>{
+    const {index} = req.params;
+    if(index >= 0 && index < alunos.length){
+      const alunoExcluido = alunos[index];
+      deletarAluno(index);
+        fs.writeFile(dbFilePath, JSON.stringify(alunos), err => {
+            if (err) {
+                res.status(500).json({message: "Erro ao escrever no arquivo"});
+            } else {
+                res.json(`${alunoExcluido.nome} você foi excluido`);
+            }
+        });
     }else {
-        res.status(400).json({message: `${aluno} não existe`})
+        res.status(400).json({message: "Índice inválido"});
     }
 });
 
 app.post("/alunos/atualizar/:index", (req, res) =>{
-    const {aluno} = req.body;
-    if(nome !== undefined ){
-        res.json(`O aluno ${aluno} foi atualizado`)
+    const {index} = req.params;
+    const {nome, matricula, media} = req.body;
+    if(index >= 0 && index < alunos.length ){
+        if(nome !== undefined && matricula !== undefined && media !== undefined){
+            const alunoAntigo = alunos[index];
+            atualizarAluno({nome, matricula, media});
+            fs.writeFile(dbFilePath, JSON.stringify(alunos), err => {
+                if (err) {
+                    alunos[index] = alunoAntigo;
+                    res.status(500).json({message: "Erro ao escrever no arquivo"});
+                } else {
+                    res.json(`O aluno ${alunoAntigo.nome} foi atualizado`);
+                }
+            });
+        } else {
+            res.status(400).json({message: "Dados inválidos"});
+        }
     }else {
-        res.status(400).json({message: `${aluno} não existe`})
+        res.status(400).json({message: "Índice inválido"});
     }
 });
 
 app.listen(3000, () => {
-    //Roda sempre que o servidor incia com sucesso
     console.log("Servidor rodando em http://localhost:3000/");
-  });
+});
